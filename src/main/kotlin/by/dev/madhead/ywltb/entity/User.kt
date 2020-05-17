fa29dbcd03e02ab64fef31bdb62379a998271382
madhead <siarhei.krukau@gmail.com>
2020-05-17 03:19:32 +0300

    Fix #6: Unlink accounts feature

--- src/main/kotlin/by/dev/madhead/ywltb/entity/User.kt
+++ src/main/kotlin/by/dev/madhead/ywltb/entity/User.kt
@@ -1,7 +1,7 @@
 package by.dev.madhead.ywltb.entity
 
 data class User(
-        val id: Int,
+        val id: Long,
         val accessToken: String,
         val refreshToken: String,
         val videosAdded: Int? = null
